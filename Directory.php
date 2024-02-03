5fc36481058017901ef97964cc882c8c82868413
Apetree333 <apetree1001@email.phoenix.edu>
2024-02-03 06:33:30 -0800

    Update and rename Directory.php to Import repository_user.sh

--- Directory.php
+++
@@ -1,9 +0,0 @@
-#title : "INDEX.MD
-Setting  up  and 
-#managing your  #GitHub profile "intro: APETREE100122 ALSO KNOWN AS ALEXANDER PETREE <APETREE1001@EMAIL.PHOENIX.EDU>
-YOU CAN ALSO SEE ME AT APETREE"
-customize #your  GitHub  profile  and  manage  your  contribution graph#short #
-	Title: Profilesredirect@
-	from : / https://github.com/Apetree100122
-#categories :/   #setting _up and managing_your    _github_profile_/github settingup _ and _ managing _ your#github#profile#versions:
-fpt: '*'  ghes: '*'  ghae: '*' ghec: '*'  topics_    Profiles.  children:  -  /   customizing-your-profile  -    /   managing-contribution-settings-on-your-profile-https://github.com/Apetree333
---
+++ Import repository_user.sh
@@ -0,0 +1,14 @@
+# title : "INDEX> 
+>Setting: import repository 
+>from "apetree100122"
+> up  
+>phone # and  # managing your # GitHub profile email "intro: APETREE100122 
+>ALEXANDER PETREE  (951)400-2505
+>Apetree333 <APETREE1001@EMAIL.PHOENIX.EDU>
+> YOU CAN ALSO  SEE ME AT   APETREE" customize 
+# your  GitHub  profile    and: manage:  your :
+    > contribution: *  graph # short # Title:
+> Profile: "Alexander petree include: apetree100122, apetree100122 , apetree, 
+> redirect: "_apetree100122* from :/  / https: 'github.com/Apetree100122_apetree333 : https://github.com/Apetree333_apetree: https://github.com/apetree'
+# categories / | *repositories |Email:  <'apetree1001@email.phoenix.edu><apetree0@gmail.com><890004155@ciat.edu>
+# setting _up and managing_your {{2FA}}, sms: 9514002505 '_github_profile_/github settingup  and_managing_ your  # github  #  profile    # versions:  fpt: '*'  ghes: '*'  ghae: '*' ghec: '*' topics_Profiles. children:  -  /   customizing-your-profile  -    /   managing-contribution-settings-on-your-profile-https://github.com/Apetree333
